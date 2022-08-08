a8cd1cbb2717b0e3dfa261b8f6a8aa005f0418bd
HoderTon <xoderton@gmail.com>
2022-08-08 22:28:07 +0300

    made it working

--- Dend/views.py
+++ Dend/views.py
@@ -1,15 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from datetime import today
+from datetime import datetime
 import json
 
 
 def index(request):
     try:
         with open('track.json', 'w') as file:
-            json.dump({'date': today.strftime("%B %d, %Y"), 'time': }
-                      |{'status_code': HttpResponse.status_code}
-                      | dict(request.headers), file)
+            json.dump({'Date': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+                       'status_code': HttpResponse.status_code} | dict(request.headers), file)
         return HttpResponse('Request saved as "track.json"')
     except:
         return HttpResponse('Error while working with file')
