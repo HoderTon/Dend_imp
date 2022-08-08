53f3f3662807077778c3e6f59093fa1466c435af
HoderTon <xoderton@gmail.com>
2022-08-08 22:19:22 +0300

    status-code and date-time

--- Dend/views.py
+++ Dend/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import today
 import json
 
 
 def index(request):
     try:
         with open('track.json', 'w') as file:
-            file.write(str(request.headers))
-        return HttpResponse('Запрос сохранён в файл track.json')
+            json.dump({'date': today.strftime("%B %d, %Y"), 'time': }
+                      |{'status_code': HttpResponse.status_code}
+                      | dict(request.headers), file)
+        return HttpResponse('Request saved as "track.json"')
     except:
-        return HttpResponse('Ошибка при работе с файлом')
+        return HttpResponse('Error while working with file')
