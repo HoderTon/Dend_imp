daf3b957879b40fb9319c986a96a0d23bee7e7b2
HoderTon <xoderton@gmail.com>
2022-08-09 14:35:49 +0300

    presets table

--- Dend/models.py
+++ Dend/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
 
-class Tank(models.Model):
-    row_1 = models.TextField
+class Crypto_Asset(models.Model):
+    pass
+
+
+class Preset(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    crypto_ass = models.ForeignKey(Crypto_Asset, on_delete=models.CASCADE)
+    coords = models.FloatField() # IDK in what format the coords should be, so for now I set float
+    text_color = models.CharField(max_length=200) # Will it be text, integer (0;255) or hexadecimal number?
+    message_text = models.TextField() # It isn't clear what kind of settings will JSON file constain
+
+
+    class Meta:
+        verbose_name = 'Preset'
+        verbose_name_plural = 'Presets'
